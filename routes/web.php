a28abc9f07fa832faa4a35ccbf9672b5f41e6655
ilham05 <ilhammaulana05303005@gmail.com>
2023-08-04 11:36:33 -0700

    menghapus yang tidak diperlukan

--- routes/web.php
+++ routes/web.php
@@ -37,37 +37,3 @@ use App\Http\Controllers\SertifikatController;
 // Route Home Page
 Route::get('/', [HomeController::class, 'index'])->name('home-page');
 
-// Authentication
-Route::middleware(['guest'])->group(function () {
-    Route::get('admin/login', [LoginController::class, 'login'])->name('login-page');
-    Route::post('admin/login', [LoginController::class, 'authenticate'])->name('authenticate');
-});Route::post('logout', [LoginController::class, 'logout'])->name('logout');
-
-// Route Admin
-Route::middleware(['auth'])->group(function () {
-    // admin page
-    Route::get('admin/admin-page', [AdminController::class, 'index'])->name('admin-page');
-
-    // blog
-    Route::get('admin/blog', [BlogController::class, 'index'])->name('admin-blog');
-    Route::get('admin/blog/create', [BlogController::class, 'create'])->name('admin-blog-create');
-    Route::post('admin/blog/store', [BlogController::class, 'store'])->name('admin-blog-store');
-    Route::get('admin/blog/edit/{blog}', [BlogController::class, 'edit'])->name('admin-blog-edit');
-    Route::post('admin/blog/update/{blog}', [BlogController::class, 'update'])->name('admin-blog-update');
-    Route::delete('admin/blog/delete/{blog}', [BlogController::class, 'destroy'])->name('admin-blog-delete');
-
-    // Sertifikat
-    Route::get('admin/sertifikat', [SertifikatController::class, 'index'])->name('admin-sertifikat');
-    Route::get('admin/sertifikat/create', [SertifikatController::class, 'create'])->name('admin-sertifikat-create');
-    Route::post('admin/sertifikat/store', [SertifikatController::class, 'store'])->name('admin-sertifikat-store');
-    Route::get('admin/sertifikat/edit/{sertifikat}', [SertifikatController::class, 'edit'])->name('admin-sertifikat-edit');
-    Route::post('admin/sertifikat/update/{sertifikat}', [SertifikatController::class, 'update'])->name('admin-sertifikat-update');
-    Route::delete('admin/sertifikat/delete/{sertifikat}', [SertifikatController::class, 'destroy'])->name('admin-sertifikat-delete');
-});
-
-// Route Blog
-Route::get('blog', [BlogController::class, 'blog'])->name('blog-page');
-
-// Route Sertifikat
-Route::get('sertifikat', [SertifikatController::class, 'sertifikat'])->name('sertifikat-page');
-
