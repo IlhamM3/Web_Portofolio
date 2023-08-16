22f652b6dbaada84f326ab680d752333ecc28f69
ilham05 <ilhammaulana05303005@gmail.com>
2023-08-16 18:40:30 +0700

    homepage index

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -32,7 +32,7 @@
             <ul class="flex flex-col p-4 mt-4 font-medium bg-gray-200 border border-gray-400 rounded-lg md:p-0 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                 <button class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" type="button">
                     <li>
-                        <a href="homepage">Home</a>
+                        <a href="{{ route('homepage') }}">Home</a>
                     </li>
                 </button>
 
